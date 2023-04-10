6af7f0a0e1e0635c896fafc8231e1bb1e604cf81
Tales Cardoso <105746645+talesbruno@users.noreply.github.com>
2023-04-10 14:55:23 -0300

    atualizações na navegação aninhada

--- app/src/main/java/co/talesbruno/mydiary/presentation/login/LoginScreen.kt
+++ app/src/main/java/co/talesbruno/mydiary/presentation/login/LoginScreen.kt
@@ -1,15 +1,14 @@
 package co.talesbruno.mydiary.presentation.login
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -77,7 +76,14 @@ fun LoginScreen(
                 }
             }
             is co.talesbruno.mydiary.domain.Result.Loading -> {
-                CircularProgressIndicator()
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color.Black.copy(alpha = 0.5f)),
+                    contentAlignment = Alignment.Center
+                ) {
+                    CircularProgressIndicator()
+                }
             }
         }
     }
